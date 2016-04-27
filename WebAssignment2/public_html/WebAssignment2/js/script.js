4dc872d69709ee2fd13dc4cf81b0239637054e05
tsgardiner <tsgardiner@windowslive.com>
2016-04-28 10:40:54 +1200

    Function Shells Created

--- WebAssignment2/public_html/WebAssignment2/js/script.js
+++ WebAssignment2/public_html/WebAssignment2/js/script.js
@@ -1,1 +1,67 @@
-//your code for Assignment 2 goes here
+
+
+//On page Load stuff if needed
+
+
+
+function OnButtonClick()
+{
+    
+}
+
+
+function MethodCalls()
+{
+    //Not sure if needed but would conditionally call each function in the steps
+    //Acts like a main driver function
+}
+
+
+
+
+
+function GetTaxData(){
+    //Wait time to get data
+}
+
+
+function CheckShippingState()
+{
+    //Change focus to unentered area
+}
+
+
+function CheckEmail()
+{
+    //Change focus to email area
+}
+
+
+function EstimateTotal()
+{
+    //Event handler
+}
+
+
+function CalculateTotalBottles()
+{
+    
+}
+
+
+function CalculateTotalShippingCost()
+{
+    
+}
+
+
+function CalculateTotalCost()
+{
+    
+}
+    
+    
+function Animation()
+{
+    
+}
