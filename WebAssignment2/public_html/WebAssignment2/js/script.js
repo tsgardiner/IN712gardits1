36da8448f2957dfd63bb70fa502b7cb7f02bf1f4
tsgardiner <tsgardiner@windowslive.com>
2016-04-28 17:56:05 +1200

    Ajax Function Complete

--- WebAssignment2/public_html/WebAssignment2/js/script.js
+++ WebAssignment2/public_html/WebAssignment2/js/script.js
@@ -1,11 +1,25 @@
 
 
-//On page Load stuff if needed
-
-
-
-function OnButtonClick()
+$(document).ready(function()
 {
+    //Jquery onload
+    //Not sure what onload stuff is needed yet
+    var btnSubmit = document.getElementById("btn-estimate");
+    
+    
+    btnSubmit.addEventListener("click", OnButtonClick);
+});
+
+
+//Globals
+var taxValue;
+
+function OnButtonClick(event)
+{
+    event.preventDefault();
+    GetTaxJSONData();
+    var output = document.getElementById("results");
+    output.innerHTML = "<p>" + taxValue + "</p>";
     
 }
 
@@ -20,8 +34,20 @@ function MethodCalls()
 
 
 
-function GetTaxData(){
-    //Wait time to get data
+
+function GetTaxJSONData(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState == 4 && request.status == 200) {
+            var states = JSON.parse(request.responseText);
+            var currentState = document.getElementById("s-state").value;           
+            taxValue = states[currentState];  
+        }
+    };
+    var taxJsonUrl = "https://dl.dropboxusercontent.com/u/10089854/Web3/Assignment2/stateTaxInfo.json";    
+    request.open("GET", taxJsonUrl, true);
+    request.send();
 }
 
 
