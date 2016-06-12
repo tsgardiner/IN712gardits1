7b9a5aa9b5c627d0ec178cf4201d9577ae8b4315
tsgardiner <tsgardiner@windowslive.com>
2016-06-12 22:38:19 +1200

    Rest Lab Finished
    Error entering data

--- RestFramework/rest/rest/urls.py
+++ RestFramework/rest/rest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('books.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
